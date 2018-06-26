8e325f3d36c0dd5b8733a02660d878a40a349976
20161104581 <a20161104581@King.local>
2018-06-26 12:00:52 +0800

    fcv
    ytvfgh

--- source/main.c
+++ source/main.c
@@ -6,10 +6,156 @@
 //  Copyright © 2018年 20161104581. All rights reserved.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <string.h>
+#define Total 5
+
+struct People {
+    int ID;
+    char name[20];
+    double score[5];
+    double final_score;
+    int rank;
+} people[Total];
+
+void inputInfo()
+{
+    printf("请输入选手信息:\n");
+    for (int i = 0; i < Total; i ++) {
+        scanf("%d", &people[i].ID);
+        getchar();
+        scanf("%s", people[i].name);
+        scanf("%lf", &people[i].score[0]);
+        scanf("%lf", &people[i].score[1]);
+        scanf("%lf", &people[i].score[2]);
+        scanf("%lf", &people[i].score[3]);
+        scanf("%lf", &people[i].score[4]);
+        people[i].final_score = 0.0;
+        people[i].rank = i + 1;
+    }
+}
+
+void calculateScore()
+{
+    for (int i = 0; i < Total; i ++) {
+        double max = people[i].score[0];
+        double min = people[i].score[0];
+        for (int j = 0; j < 5; j ++) {
+            people[i].final_score += people[i].score[j];
+            if (people[i].score[j] > max)
+                max = people[i].score[j];
+            if (people[i].score[j] < min)
+                min = people[i].score[j];
+        }
+        people[i].final_score = people[i].final_score - max - min;
+        people[i].final_score /= 3;
+    }
+    printf("分数计算成功!\n");
+}
+
+void sort()
+{
+    calculateScore();
+    for (int i = 0; i < Total - 1; i ++) {
+        for (int j = 0; j < Total - 1 - i; j ++) {
+            if (people[j].final_score < people[j + 1].final_score && people[j].rank < people[j + 1].rank) {
+                struct People tmp = people[j];
+                people[j] = people[j + 1];
+                people[j + 1] = tmp;
+                people[j].rank = j + 1;
+                people[j + 1].rank = j + 2;
+            }
+            if (people[j].final_score > people[j + 1].final_score && people[j].rank > people[j + 1].rank) {
+                struct People tmp = people[j];
+                people[j] = people[j + 1];
+                people[j + 1] = tmp;
+                people[j].rank = j + 1;
+                people[j + 1].rank = j + 2;
+            }
+        }
+    }
+    printf("排序成功!\n");
+}
+
+void searchByID()
+{
+    int ID;
+    printf("请输入id:");
+    scanf("%d", &ID);
+    for (int i = 0; i < Total; i ++) {
+        if (people[i].ID == ID) {
+            printf("-----------------------------------------\n");
+            printf("--  你要查找的人 No.%d     \n", ID);
+            printf("--  最终分数: %.2lf            \n", people[i].final_score);
+            printf("--  排名为: %d                      \n", people[i].rank);
+            printf("-----------------------------------------\n");
+            return;
+            
+        }
+    }
+}
+void searchByName()
+{
+    char name[20];
+    printf("请输入姓名:");
+    scanf("%s", name);
+    for (int i = 0; i < Total; i ++) {
+        if (strcmp(people[i].name, name) == 0) {
+            printf("-----------------------------------------------\n");
+            printf("--  你要查找的人: %s       \n", name);
+            printf("--  最终分数: %.2lf            \n", people[i].final_score);
+            printf("--  排名为: %d                      \n", people[i].rank);
+            printf("-----------------------------------------------\n");
+            return;
+            
+        }
+    }
+    
+}
+
+void quertPeople()
+{
+    int choice;
+    printf("根据姓名查找还是编号查找? 0-ID,1-name:");
+    scanf("%d", &choice);
+    if (choice == 0)
+        searchByID();
+    if (choice == 1)
+        searchByName();
+}
+
+void menu()
+{
+    printf("-------------------------------------------------\n");
+    printf("--  1 - 输入选手信息                             --\n");
+    printf("--  2 - 计算分数并排序数据  --\n");
+    printf("--  3 - 查询选手                     --\n");
+    printf("--  0 - 退出                                  --\n");
+    printf("-------------------------------------------------\n");
+    
+}
+
+int main() {
+    int choice;
+    menu();
+    scanf("%d", &choice);
+    while (choice != 0){
+        switch (choice) {
+            case 1:
+                inputInfo();
+                break;
+            case 2:
+                sort();
+                break;
+            case 3:
+                quertPeople();
+                break;
+            default:
+                break;
+        }
+        menu();
+        scanf("%d", &choice);
+    }
     return 0;
 }
