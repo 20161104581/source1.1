700f4167223b66b230bb037331fbaf330caf9f9a
20161104581 <a20161104581@bogon>
2018-06-26 10:32:03 +0800

    Initial Commit

---
+++ source/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  source
+//
+//  Created by 20161104581 on 18/6/26.
+//  Copyright © 2018年 20161104581. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
